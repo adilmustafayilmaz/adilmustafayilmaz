2b51a449de876c28fa9c17eb542b87788d0669e2
Adil Mustafa Yilmaz <a.mustafayilmaz02@gmail.com>
2024-01-03 11:34:06 +0300

    TypeerEffect added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+'use client'
 import Image from 'next/image'
+import Typewriter from 'typewriter-effect'
 
 export default function Home() {
   return (
     <>
       <div className='h-screen flex flex-col justify-center items-center'>
-        <h1>ADİL MUSTAFA YILMAZ</h1>
-        <div>Coming soon ...</div>
+        <Typewriter
+          options={{
+            strings: "Adil Mustafa Yılmaz",
+            autoStart: true,
+            loop: false,
+          }} />
+
+          <Typewriter options={{
+            strings: "Coming Soon ....",
+            autoStart: true,
+            loop: false
+          }}/>
+
+
       </div>
     </>
   )
